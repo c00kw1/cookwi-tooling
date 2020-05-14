bdc65ce8656aac5abcd9a6baf5ef16e3ba879a44
gjdass <gustave@dassonville.io>
2020-05-15 01:42:06 +0200

    order matter in term of services restarts

--- server-setup/webhook/commands/pull-tooling.sh
+++ server-setup/webhook/commands/pull-tooling.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# !!!! IMPORTANT !!!!
+# The version of this file used by webhooks is in fact located in /var/scripts on the server.
+# It means this file (and only this one) has to be updated MANUALLY.
+# Updating it itself would kill all the CI ...
+#
+
 cd /root/cookwi-tooling
 git fetch
 # do we need to pull and build and deploy ?
@@ -13,16 +19,16 @@ if [ $LOCAL = $REMOTE ]; then # up-to-date
 elif [ $LOCAL = $BASE ]; then # we need to pull
     git pull
 
+    # deploy API SQL credentials
+    cp ./server-setup/credentials/cookwi-sql-credentials.json /var/www/cookwi-api/credentials
+    service cookwi-api restart
+
     # deploy Webhooks
     cp -r ./server-setup/webhook /var/www/webhook-tmp
     chmod +x /var/www/webhook-tmp/commands/*.sh
     rm -rf /var/www/webhook
     mv /var/www/webhook-tmp /var/www/webhook
     service webhook restart
-
-    # deploy API SQL credentials
-    cp ./server-setup/credentials/cookwi-sql-credentials.json /var/www/cookwi-api/credentials
-    service cookwi-api restart
 fi
 
 exit 0
